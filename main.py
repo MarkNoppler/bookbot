4ed4e7f46024bd336c3f5c9a72eab814dfa365ed
MarkNoppler <cappslohk@hotmail.com>
2024-06-17 14:51:02 +0100

    Alphabetically ordered the character count

--- main.py
+++ main.py
@@ -5,6 +5,15 @@ def main():
 	print(f"Total words: {total_words}")
 	character_counts = count_characters(text)
 	print(f"Character Counts: {character_counts}")
+	letters = [{char: count} for char, count in character_counts.items() if char.isalpha()]
+	letters.sort(reverse=True, key=sort)
+	print("--- Begin report of books/frankenstein.txt ---")
+	print(f"{total_words} words found in the document\n")
+
+	for letter_dict in letters:
+		char, count = list(letter_dict.items())[0]
+		print(f"The '{char}' character was found {count} times")
+	print("--- End report ---")
 	
 def get_book_text(path):
 	with open(path) as f:
@@ -24,6 +33,8 @@ def count_characters(text):
 			char_count[char] = 1
 	return char_count
 
+def sort(dict):
+	return list(dict.values())[0]
 
-
-main()
+if __name__ == "__main__":
+	main()
